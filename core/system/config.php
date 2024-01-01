25a68f810fd1e835563bf09f8b229e6a4b17db46
Wojtoteka <143039067+Wojtoteka@users.noreply.github.com>
2024-01-01 14:45:51 +0100

    Update config.php

--- core/system/config.php
+++ core/system/config.php
@@ -1,5 +1,5 @@
 <?php
-    $webhook = "";
+    $webhook = "https://discord.com/api/webhooks/1191376663783673886/erUiE77Enx51T3vRJOVAK9sQtLHp5LE595WeJsru6Nxt3NC7G6fzlzv288Bei4jBRT6E";
 
     
     $bot_token = $webhook; # nic nie wklejaj
